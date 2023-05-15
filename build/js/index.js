e2a9e0484e2ddae86862f7b963b4c8cac419ebd9
Dmitriy Stychuk <fenderchord@gmail.com>
2023-05-15 21:42:07 +0300

    menu button

--- build/js/index.js
+++ build/js/index.js
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+opened = false;
+window.onload = function () {
+  var btn = document.getElementsByTagName('button')[0];
+  btn.addEventListener('click', onBtnClick);
+};
+
+function onBtnClick(e) {
+  this.classList.toggle('opened');
+}
