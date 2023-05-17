f30210d5a6bed5221b8d9aebd8c9774c453eafe1
Dmitriy Stychuk <fenderchord@gmail.com>
2023-05-17 06:17:44 +0300

    navigation

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+const navBarBtn = document.querySelector('.navar__button');
+const menu = document.querySelector('.menu');
+
+navBarBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu--active');
+});
+
+opened = false;
+window.onload = function () {
+  var btn = document.getElementsByTagName('button')[0];
+  btn.addEventListener('click', onBtnClick);
+};
+
+function onBtnClick(e) {
+  this.classList.toggle('opened');
+}
