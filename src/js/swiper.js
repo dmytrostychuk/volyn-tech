115e69ef12c1c747539bf2c3a443a15a50c93ca7
Dmitriy Stychuk <fenderchord@gmail.com>
2023-06-19 13:53:12 +0300

    add title

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -4,7 +4,26 @@ var swiper = new Swiper('.mySwiper', {
   slidesPerView: 4,
   freeMode: false,
   watchSlidesProgress: true,
+
+  breakpoints: {
+    '@0.00': {
+      slidesPerView: 3,
+    },
+    '@0.75': {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    '@1.00': {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    '@1.50': {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
 });
+
 var swiper2 = new Swiper('.mySwiper2', {
   loop: true,
   spaceBetween: 10,
