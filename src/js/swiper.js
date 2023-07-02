ba6981228455cc58aaa70d4d6ac4d6553e84b499
Dmitriy Stychuk <fenderchord@gmail.com>
2023-07-02 22:56:31 +0300

    add header

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,32 +1,28 @@
 var swiper = new Swiper('.mySwiper', {
-  loop: true,
+  // loop: true,
   spaceBetween: 10,
   slidesPerView: 4,
-  freeMode: false,
-  watchSlidesProgress: true,
+
+  // watchSlidesProgress: true,
 
   breakpoints: {
     '@0.00': {
-      slidesPerView: 3,
+      slidesPerView: 2,
     },
     '@0.75': {
       slidesPerView: 3,
-      spaceBetween: 20,
     },
     '@1.00': {
       slidesPerView: 4,
-      spaceBetween: 40,
     },
     '@1.50': {
       slidesPerView: 4,
-      spaceBetween: 50,
     },
   },
 });
 
 var swiper2 = new Swiper('.mySwiper2', {
   loop: true,
-  spaceBetween: 10,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
